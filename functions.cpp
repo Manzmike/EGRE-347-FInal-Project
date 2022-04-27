d238696d7e8c52426edb9d80a608b1f5dc0e7d67
Michael Lindsay <mikelindsayengineering@gmail.com>
2022-04-27 16:03:23 -0400

    Adding description

--- functions.cpp
+++ functions.cpp
@@ -1,1 +1,27 @@
-function
+//functions
+
+//LED function
+// Schuld fucntion
+//Importing Data
+//2d array 7 x 24 
+
+
+
+
+//include menu
+
+//LEDS 
+
+//adding to 
+
+
+//removing from
+
+
+// Exporting
+
+
+//importing
+
+
+
---
+++ main.cpp
@@ -0,0 +1,18 @@
+// Main file
+
+//Uses Classes
+//Uses Menu
+// Add events
+// looks at events
+// Importing events
+// export events
+// Importing a time lib for 
+
+
+// Making looking a actual time schu
+
+
+//Takes time and if time is off then then tells user to exit and do touch *
+
+//Ical/GCal exporting
+//
